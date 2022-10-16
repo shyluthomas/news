77f80eadfed905dc1ec2f9535276977c750de725
shyluthomas <shylupthomas@gmail.com>
2022-10-16 17:13:19 +0530

    The final changes css
    Signed-off-by: shyluthomas <shylupthomas@gmail.com>

--- src/components/main/SideOne.js
+++ src/components/main/SideOne.js
@@ -1,67 +1,82 @@
 import React from 'react'
-import data from './data.json'
 import AddIcon from '@mui/icons-material/Add';
 import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
 import AddCommentIcon from '@mui/icons-material/AddComment';
 
-function SideOne() {
+function SideOne({newsCollection}) {
+let primeNews = newsCollection?.filter((item) => item.newsRank === 1);
+let topNews = newsCollection?.filter((item) => item.newsRank === 2);
+let normalNews = newsCollection?.filter((item) => item.newsRank === 0);
     return (
         <div className='side-one'>
             <div className='side-one-top'>
                 Coming Soon
             </div>
             <div className='side-one-bottom'>
+            {
+              primeNews ?
                 <div className='side-one-bottom-left'>
-                    <img src="https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="a man" height="500px" width="400px" />
-                    <div className='news1-heading'>LIVE: GREG INGLIS TO ANNOUNCE NRL RETIREMENT</div>
+                    <img src={primeNews[0].image[0]}  alt="a man"  />
+                    <div className='news1-heading'>{primeNews[0].title}</div>
                     <p className='news-title'>
-                        <span >RABBITOHS STAR</span> Greg Inglis has arrived at South Sydney headquaters and will front a media conference later this morning to announce his retirement from rugby league
+                        <span className='starter'>{primeNews[0].starter.toUpperCase()}</span> 
+                        {primeNews[0].introduction}
                     </p>
                     <div className='slideBottomIcons'>
                             <div className='icon-alignments'>
-                                <AccessTimeFilledIcon></AccessTimeFilledIcon>2m
+                                <AccessTimeFilledIcon fontSize="small"></AccessTimeFilledIcon>{primeNews[0].published}
                             </div>
                             <div  className='icon-alignments ml-5'>
-                                <AddCommentIcon></AddCommentIcon>2m
+                                <AddCommentIcon fontSize="small"></AddCommentIcon>2m
                             </div>
                     </div>
-                </div>
+                </div> :
+                <span>News is not available</span>
+            }
                 <div className='side-one-bottom-right'>
+                { topNews ? 
                     <div>
                         <div className='side-one-btm-rht-imagecol'>
-                            <img src="https://st1.photogallery.ind.sh/wp-content/uploads/indiacom/zozibini-tunzi-top-20-most-beautiful-women-in-the-world-2022-202003-1655117381.jpg?impolicy=Medium_Widthonly&w=450" alt='woman1' height="200px" />
-                            <img src="https://images.pexels.com/photos/1024311/pexels-photo-1024311.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt='woman2' height="200px" />
+                        { topNews[0].image.map((item) => {
+                            return (<img src={item} alt="norm" width='50%' ></img>)
+                        })}
+                           
                         </div>
                         <span className='news1-heading'>
-                            Opal Tower Units still closed as work only approved now
+                        {topNews[0].title}
                         </span>
-                        <p style={{ fontWeight: 500, fontSize: '0.9rem' }}><span style={{ color: 'blue', fontWeight: 700 }}> ESTABLISHED</span> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+                        <p className='news-title-top'><span className='starter-top' > {topNews[0].starter.toUpperCase()}</span>
+                        {topNews[0].introduction}
+                         </p>
                         <div className='sub-icons'>
                             <div className='icon-alignments'>
-                                <AccessTimeFilledIcon></AccessTimeFilledIcon>2m
+                                <AccessTimeFilledIcon fontSize="small"></AccessTimeFilledIcon>{topNews[0].published}
                             </div>
                             <div  className='icon-alignments ml-5'>
-                                <AddCommentIcon></AddCommentIcon>2m
+                                <AddCommentIcon fontSize="small"></AddCommentIcon>2m
                             </div>
                         </div>
-                    </div>
+                    </div> :
+                    <span>News is not available</span>
+                    }
                     {
-                        data.map((item) => {
+                        normalNews ? normalNews.map((item) => {
                             return (
                                 <div key={item.id} className="side-one-bottom-right-newssection">
                                     <div className='side-one-bottom-right-post'>
                                         <div className='post-head'>
                                             <AddIcon fontSize="small"></AddIcon>
-                                            <span>{item.description}</span>
+                                            <span> {item.title}</span>
                                         </div>
-                                        <img src={item.image} alt="news" width="100px" height="80px" />
+                                        <img src={item.image[0]} alt="news" width="100px" height="80px" />
                                     </div>
                                     <div className='icon-alignments border-bottom-cut'>
-                                        <AccessTimeFilledIcon></AccessTimeFilledIcon>{item.poston}
+                                        <AccessTimeFilledIcon fontSize="small"></AccessTimeFilledIcon>{item.published}
                                     </div>
                                 </div>
                             )
-                        })
+                        }) :
+                        <span>News is not available</span>
                     }
 
                 </div>
--- src/views/Main.js
+++ src/views/Main.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { FetchLatestNews, FetchNewsCollection} from '../store/news/NewsSlice';
 import SideOne from "../components/main/SideOne";
 import SideTwo from "../components/main/SideTwo";
 import './main.scss'
+import Loader from "../layouts/loader/Loader";
 
 const Main = () => {
+
+  const dispatch = useDispatch();
+  const latestNews = useSelector((state) => state.news.latestNews)
+  const newsCollection = useSelector((state) => state.news.newsCollection);
+  useEffect(() => {
+    dispatch(FetchLatestNews(true));
+  },[]);
+  useEffect(() => {
+    if(latestNews.length > 0) {
+      const homeData = latestNews.filter((item) => item.collectiontype === 'home');
+      console.log('homeData,',homeData[0].collectionid);
+      if(homeData.length > 0) {
+        dispatch(FetchNewsCollection(homeData[0]?.collectionid));
+      }       
+    }
+  },[latestNews]);
+
+  console.log('first,',latestNews)
   return (
     <div className="App">
-          <SideOne/>
+        { newsCollection.length > 0 ?
+            <SideOne newsCollection = {newsCollection}></SideOne>
+            :
+            <Loader></Loader>
+          }
           <SideTwo/>
     </div>
   );
